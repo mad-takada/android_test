449b528df2df9813ddf3ac12b21da2321d4a8d05
unknown <>
2016-08-14 17:38:11 +0900

    add rxjava

--- MyApplication/app/src/main/java/com/example/myapplication/bus/EventBusHolder.java
+++ MyApplication/app/src/main/java/com/example/myapplication/bus/EventBusHolder.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.myapplication.bus;
 
 import com.squareup.otto.Bus;
 
--- MyApplication/app/src/main/java/com/example/myapplication/ui/OttoFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/OttoFragment.java
@@ -1,36 +1,47 @@
-package com.example.myapplication;
+package com.example.myapplication.ui;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.myapplication.bus.ButtonClickEvent;
+import com.example.myapplication.bus.EventBusHolder;
+import com.example.myapplication.R;
+
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class OttoActivityFragment extends Fragment {
+public class OttoFragment extends Fragment {
 
     @BindView(R.id.vid_button) Button button1;
 
     @BindString(R.string.app_name) String app_name;
 
+    private Unbinder unbinder;
     private int mClickCount;
 
-    public OttoActivityFragment() {
+    public OttoFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.otto_fragment, container, false);
-        ButterKnife.bind(this, view);
+        unbinder = ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 
     @OnClick(R.id.vid_button)
